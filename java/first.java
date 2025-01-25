9b9da3d15d64f384db07f334c3beeac830585d5c
nikhil-kumar490 <927nikhilkumar@gmail.com>
2025-01-25 21:12:50 +0530

    boolen

--- java/first.java
+++ java/first.java
@@ -4,3 +4,18 @@ class HelloWorld {
         System.out.println("Hello World");
     }
 }
+// Data Type
+
+class HelloWorlds {
+    public static void main(String[] b) {
+       String n = "nikhil";
+        System.out.println("n");
+    }
+}
+
+class second{
+    public static void main(String[] c){
+        Boolean n= true;
+        System.out.println(n);
+    }
+}
