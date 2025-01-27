62c8271ac752de71c34df89caf913835a0ee8ea5
nikhil-kumar490 <927nikhilkumar@gmail.com>
2025-01-27 21:21:05 +0530

    LargestNumber

--- java/Larges_Number.java
+++ java/Larges_Number.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter a number: ");
+        System.out.print("Enter the number: ");
         int num = scanner.nextInt();
 
         int reverse = 0;
