1bf45de589d97a1a7187969c8812e5cce6313f8d
nikhil-kumar490 <927nikhilkumar@gmail.com>
2025-01-28 20:56:40 +0530

    cart

---
+++ java/casting.java
@@ -0,0 +1,13 @@
+ class casting {
+     public static void main(String[] args) {
+//         char c='A';
+//         char c='*';
+         char c='&';
+         int num =c;
+         System.out.println(num);
+
+         int n=38;
+         char st=(char)n;
+         System.out.println(st);
+     }
+}
