95d55a7db23fd74e0049c1ec22841329f1fcc99c
nikhil-kumar490 <927nikhilkumar@gmail.com>
2025-01-28 20:40:43 +0530

    all

--- java/Reverse.java
+++ java/Reverse.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
- class ReverseNumber {
+ class Reverse {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
---
+++ java/main.java
@@ -0,0 +1,16 @@
+class main {
+    public static void main(String[] a){
+//        float n= 5;
+//        double n= num;
+//        long n= num;
+//        byte n= 3;
+//        double n= 11.9999;
+//        int num=(int)n;
+
+        int num=130;
+        byte r=(byte)num;
+
+        System.out.println(r);
+    }
+
+}
