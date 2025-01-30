f8c1de22a25215191e7566e5650d021e3a6a5d3e
nikhil-kumar490 <927nikhilkumar@gmail.com>
2025-01-30 10:28:45 +0530

    Operators all r

--- java/Operators.java
+++ java/Operators.java
@@ -1,5 +1,7 @@
 public class Operators {
     public static void main(String[] args) {
+
+      // Relational Opretor----
         int a=5 ;
         int b=6;
         System.out.println(a==b);
@@ -8,6 +10,21 @@ public class Operators {
         System.out.println(a>=b);
         System.out.println(a<=b);
         System.out.println(a!=b);
+
+// Logical Opretors------
+
+//        boolean c= true;
+//        boolean d= false;
+//        boolean d= true;
+        int c=2;
+        int d=3;
+        int e=2;
+        //&&
+        System.out.println(c==d && c==e);
+
+        // \\
+        System.out.println(c==d || c==e);
+
 
     }
 
