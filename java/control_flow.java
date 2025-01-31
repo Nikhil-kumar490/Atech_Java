49f81d75fed73fba349fa68cec2f7fb12ecc3262
nikhil-kumar490 <927nikhilkumar@gmail.com>
2025-01-31 20:22:20 +0530

    small

--- java/control_flow.java
+++ java/control_flow.java
@@ -1,5 +1,18 @@
 public class control_flow {
     public static void main(String[] args) {
-
+        int i=4;
+        if (i<=10){
+            if (i<=5){
+                System.out.println("yeh");
+            }else {
+                System.out.println("No");
+            }
+            System.out.println("Small");
+        } else if(i<=15){
+            System.out.println("medium");
+        }
+        else {
+            System.out.println("big");
+        }
     }
 }
