c5d90fc8b33a809bb4be6d852f4429aaec6d7efa
nikhil-kumar490 <927nikhilkumar@gmail.com>
2025-01-31 21:09:55 +0530

    small

---
+++ java/loop.java
@@ -0,0 +1,14 @@
+// FOR LOOP----(1-initialization, 2-Condtion , 3-Update the expresion , 4-Body / code part )
+
+import java.util.Scanner;
+
+public class loop {
+    public static void main(String[] args) {
+        Scanner st= new Scanner(System.in);
+        System.out.print("Enter a value");
+        int num = st.nextInt();
+        for(int i=1; i<=10; i++){
+            System.out.println();
+        }
+    }
+}
