314e2c26c75d1178e3ada3e9839e4e55ba0efbde
nikhil-kumar490 <927nikhilkumar@gmail.com>
2025-02-05 19:29:16 +0530

    week

---
+++ java/switch_2.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class switch_2 {
+    public static void main(String[] args) {
+        Scanner nk= new Scanner(System.in);
+        System.out.print("Enter a num : ");
+        int m= nk.nextInt();
+        switch(m){
+
+            case 1:
+                System.out.println("Sunday");
+                break;
+            case 2:
+                System.out.println("Monday");
+                break;
+            case 3:
+                System.out.println("Tuseday");
+                break;
+            case 4:
+                System.out.println("Wednesday");
+                break;
+            case 5:
+                System.out.println("Thursday");
+                break;
+            case 6:
+                System.out.println("Friday");
+                break;
+            case 7:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Wrong number");
+        }
+    }
+}
