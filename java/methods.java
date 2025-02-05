ce6fe6d6c6b29c017abbfbed090a5b75d9325bdb
nikhil-kumar490 <927nikhilkumar@gmail.com>
2025-02-05 20:36:01 +0530

    add

---
+++ java/methods.java
@@ -0,0 +1,13 @@
+public class methods {
+    public static void main(String[] args) {
+        int a=4;
+        int b= 6;
+        sum(a,b);
+    }
+
+    public static int sum (int a , int b){
+        System.out.println(a+b);
+        return 0;
+
+    }
+}
