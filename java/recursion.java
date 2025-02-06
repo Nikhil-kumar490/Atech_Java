cca576745998d9e41f5be5440f191491005141cd
nikhil-kumar490 <927nikhilkumar@gmail.com>
2025-02-06 20:53:52 +0530

    switch

--- java/recursion.java
+++ java/recursion.java
@@ -1,2 +1,20 @@
 public class recursion {
+    public static void main(String[] args) {
+        // Factorial
+
+        int i=7;
+        System.out.println(+factorial(i));
+    }
+    public static int factorial(int i){
+        if (i==0){
+            //Base case
+            return 1;
+        } else{
+            return i * factorial(i-1);// recrsive case
+
+            // for sum of natural number
+//            return i * factorial(i-1);// recrsive case
+
+        }
+    }
 }
