adbff0b4cb5d52e19148fab3334d315900db2017
nikhil-kumar490 <927nikhilkumar@gmail.com>
2025-02-07 21:04:40 +0530

    switch

---
+++ java/opps_1.java
@@ -0,0 +1,15 @@
+public class opps_1 {
+    public static void main(String [] a){
+       Car c1=new Car();
+       c1.model= "carx1";
+       c1.color="red";
+        c1.launchYear=2015;
+        c1.display();
+    }
+}
+ class Car{
+    String model;
+    String color;
+    int launchYear;
+    System.out.println("Model= "+model+"Color="+color+ "Lainch on= "+launchYear);
+ }
