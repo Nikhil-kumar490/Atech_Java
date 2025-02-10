2fc7a8d27e25fd96783dfd334e945bcf22e19b06
nikhil-kumar490 <927nikhilkumar@gmail.com>
2025-02-10 20:57:24 +0530

    my

---
+++ java/oops_2.java
@@ -0,0 +1,9 @@
+public class oops_2 {
+    public static void main(String[] a) {
+        Car c1 = new Car();
+//        c1.model = "carx1";
+//        c1.color = "red";
+//        c1.launchYear = 2015;
+        c1.display();
+    }
+}
