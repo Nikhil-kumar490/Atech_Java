bf15c772ae8dd9b9e671d7784b90875f5fa4cf76
nikhil-kumar490 <927nikhilkumar@gmail.com>
2025-02-11 20:55:51 +0530

    my

--- java/inheritance_1.java
+++ java/inheritance_1.java
@@ -1,15 +1,15 @@
 public class inheritance_1 {
     public static void main(String[] args) {
      Honda h1=new Honda();
-     h1.name_of_car("HondaX");
+     h1.name_of_car();
     }
 }
 
 class  Carr{
     String car_name;
-    void name_of_car( String car_name){
-        this.car_name= car_name;
-        System.out.println("car is"+car_name);
+    void name_of_car(){
+        this.car_name= "HondaX";
+        System.out.println("car is"+ "HondaX");
     }
 }
 class Honda extends Carr{
