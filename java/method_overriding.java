0f82e72d7e02fed6fe673146a1886b5683920ff7
nikhil-kumar490 <927nikhilkumar@gmail.com>
2025-02-13 20:32:43 +0530

    my

---
+++ java/method_overriding.java
@@ -0,0 +1,20 @@
+public class method_overriding {
+    public static void main(String[] args) {
+      Dogs d = new Dogs();
+      d.sound();
+
+      Animals s = new Animals();
+        s.sound();
+    }
+}
+class Animals{
+    void sound(){
+        System.out.println("Sound");
+    }
+
+} class Dogs extends Animals{
+    @Override
+     void sound(){
+         System.out.println("Bhow");
+     }
+}
