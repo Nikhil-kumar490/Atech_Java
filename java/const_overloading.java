e8ed6a44758700ee47fd3a0445fdf00165abbf63
nikhil-kumar490 <927nikhilkumar@gmail.com>
2025-02-13 21:01:51 +0530

    my

---
+++ java/const_overloading.java
@@ -0,0 +1,20 @@
+public class const_overloading {
+    public static void main(String[] a){
+        T t= new T();
+    }
+}
+class T{
+    int i;
+    T(){
+        this("A");
+        System.out.println("this is me");
+    }
+    T(int i){
+        this.i =i;
+        System.out.println(i);
+    }
+    T(String s){
+//        this(5);  // this sequencce is following recursion situation thats why it not allowed
+        System.out.println(s);
+    }
+}
