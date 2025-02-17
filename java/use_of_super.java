cf67e6f07a7c8fb6ab37966bce197d8d201a05ec
nikhil-kumar490 <927nikhilkumar@gmail.com>
2025-02-17 20:27:29 +0530

    my

---
+++ java/use_of_super.java
@@ -0,0 +1,22 @@
+public class use_of_super {
+    public static void main(String[] args) {
+//        P p =new P();
+//        p.M();
+        Cs s= new Cs();
+        s.M();
+    }
+}
+class P{
+
+//    void M(){
+//        System.out.println("i am P ");
+//    }
+
+}
+class Cs extends P{
+//    @Override
+//    void M(){
+//        super.M();
+//        System.out.println("i am Cs");
+//    }
+}
