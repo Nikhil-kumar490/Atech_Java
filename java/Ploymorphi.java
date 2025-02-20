0577925ddf90054e966a83b040ea00b4ccccd2a7
nikhil-kumar490 <927nikhilkumar@gmail.com>
2025-02-20 20:29:14 +0530

    my

---
+++ java/Ploymorphi.java
@@ -0,0 +1,11 @@
+public class Ploymorphi {
+    public static void main(String[] args) {
+        AC a= new AC();
+        a.M(5);
+    }
+}
+class AC{
+    void M(int i){
+        System.out.println();
+    }
+}
