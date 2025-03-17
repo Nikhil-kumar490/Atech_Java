3344784003391c512480b4387a913c382e20ce72
nikhil-kumar490 <927nikhilkumar@gmail.com>
2025-03-17 18:58:13 +0530

    start4

--- java/array_1.java
+++ java/array_1.java
@@ -11,7 +11,7 @@ public class array_1 {
         arr[1]=2;
         arr[2]=7;
 
-        // to string
+        // 1 -> to string
         int[] arr1 = {2,3,5,6,8,9};
 
         System.out.println(Arrays.toString(arr1));
@@ -26,26 +26,48 @@ public class array_1 {
             System.out.println(n);
         }
 
-        // for sort
+        // 2 -> for sort
         Arrays.sort(num);
         System.out.println(Arrays.toString(num));
 
-        //for fill
+        // 3 -> for fill
         int[] arr2= new int[5];
         Arrays.fill(arr2, 10);
         System.out.println(Arrays.toString(arr2));
 
-        // for copy
+        // 4 -> for copy
         int[] original={2,6,3,7,9,5,8};
         int [] copied= Arrays.copyOf(original,4);
         System.out.println(Arrays.toString(original));
         System.out.println(Arrays.toString(copied));
 
-        // for range
+        // 5 -> for range
         int[] originals={2,6,3,7,9,5,8};
         int [] copiedd= Arrays.copyOfRange(originals,2,5);
         System.out.println(Arrays.toString(originals));
         System.out.println(Arrays.toString(copiedd));
+
+        // 6 -> for Equal
+        int[] eql1={1,2,3,4,5};
+        int[] eql2={1,3,4,2,5};
+        int[] eql3={1,2,3,4,5};
+
+        System.out.println(Arrays.equals(eql1,eql2));
+        System.out.println(Arrays.equals(eql1,eql3));
+
+        // 7 -> Binay search
+        int[] src1={1,2,3,4,5};
+        int[] src2={1,3,4,2,5};
+        int[] src3={1,12,3,14,5,2,6,33,0,19};
+        int index= Arrays.binarySearch(src3, 2);
+        System.out.println(index);
+
+        // 8 -> arrayCopy
+        int[] s={10,20,30,40,50};
+        int[] d= new int[5];
+        System.arraycopy(s,1,d,0,3);
+        System.out.println(Arrays.toString(d));
+
     }
 
 }
