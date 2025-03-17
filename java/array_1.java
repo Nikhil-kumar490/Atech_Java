234f8042b3ca84ee2fde654b9c29ffd780334c2d
nikhil-kumar490 <927nikhilkumar@gmail.com>
2025-03-17 17:46:55 +0530

    start

---
+++ java/array_1.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class array_1 {
+    public static void main(String[] arrgs){
+        int arr[];
+
+        arr = new int[6];
+        arr[0]=5;
+        arr[1]=2;
+        arr[2]=7;
+
+        int[] arr1 = {2,3,5,6,8,9};
+
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(arr));
+        System.out.println(arr[2]);
+    }
+
+}
--- java/method_overriding.java
+++ java/method_overriding.java
@@ -18,3 +18,5 @@ class Animals{
          System.out.println("Bhow");
      }
 }
+
+
