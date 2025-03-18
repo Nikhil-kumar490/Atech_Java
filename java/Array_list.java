675ab5a5540b6b0870ac245c17e1bf12788adca9
nikhil-kumar490 <927nikhilkumar@gmail.com>
2025-03-18 20:49:37 +0530

    list

---
+++ java/Array_list.java
@@ -0,0 +1,10 @@
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+
+public class Array_list {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Apple");
+        System.out.println(list);
+    }
+}
