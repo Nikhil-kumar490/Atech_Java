c5635e64b1f739447e243cddcf6abc3b31821ccd
nikhil-kumar490 <927nikhilkumar@gmail.com>
2025-03-18 19:14:40 +0530

    start5

--- java/t2D_array.java
+++ java/t2D_array.java
@@ -1,2 +1,11 @@
 public class t2D_array {
+    public static void main(String[] args) {
+        int[] arr1 ={3,2,4,5,7,6,8};
+        int[] arr2 ={2,4,7,3,8,5,6};
+        int[] output = new int[4];
+
+        for (int i=0; i<arr1.length; i++){
+            output[i]= arr1[i]+arr2[i];
+        }
+    }
 }
