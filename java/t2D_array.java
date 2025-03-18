3ec4381e6c3a81bb35afec5a5f6d7a153492734a
nikhil-kumar490 <927nikhilkumar@gmail.com>
2025-03-18 19:17:39 +0530

    start5

--- java/t2D_array.java
+++ java/t2D_array.java
@@ -1,11 +1,15 @@
+import java.util.Arrays;
+// Adding The 2 Arrays crosponding  values in new array list
+
 public class t2D_array {
     public static void main(String[] args) {
         int[] arr1 ={3,2,4,5,7,6,8};
         int[] arr2 ={2,4,7,3,8,5,6};
-        int[] output = new int[4];
+        int[] output = new int[7];
 
         for (int i=0; i<arr1.length; i++){
             output[i]= arr1[i]+arr2[i];
         }
+        System.out.println(Arrays.toString(output));
     }
 }
