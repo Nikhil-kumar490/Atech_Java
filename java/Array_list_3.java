e59db055851d6fea7f1996be9d868595057377d2
nikhil-kumar490 <927nikhilkumar@gmail.com>
2025-03-19 20:59:42 +0530

    list

---
+++ java/Array_list_3.java
@@ -0,0 +1,23 @@
+
+    import java.sql.SQLOutput;
+import java.util.ArrayList;
+
+public class Array_list_3 {
+        public static void main(String[] args) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add("A");
+            list.add(0,"E");
+            list.add("B");
+            list.remove(1);
+            list.add("c");
+            list.add(1,"D");
+            list.remove(3);
+            list.set(2,"H");
+            list.add("F");
+            list.add(3,"G");
+
+            System.out.println(list);
+        }
+    }
+
+
