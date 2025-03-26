a32ee760a8292a9fa5e5ac77b0f1a48ebf0ab856
nikhil-kumar490 <927nikhilkumar@gmail.com>
2025-03-26 20:51:11 +0530

    list

---
+++ java/string.java
@@ -0,0 +1,9 @@
+public class string {
+    public static void main(String[] args) {
+        String s = "This is Me";
+        System.out.println(s);
+        String s2 = "java";
+        char c= s.charAt(1);
+        System.out.println(c);
+    }
+}
