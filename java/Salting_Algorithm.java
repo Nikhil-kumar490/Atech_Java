a027ba75431ca69db536c7f03e31cf685c8bb0c1
nikhil-kumar490 <927nikhilkumar@gmail.com>
2025-03-27 21:22:02 +0530

    string

---
+++ java/Salting_Algorithm.java
@@ -0,0 +1,22 @@
+
+    import java.util.Scanner;
+
+    public class Salting_Algorithm {
+        public static String addSalt(String word) {
+            String saltedWord = "";
+
+            for (int i = 0; i < word.length(); i++) {
+                saltedWord = saltedWord + word.charAt(i) + "s";
+            }
+
+            return saltedWord; // Return the final salted string
+        }
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter a word: ");
+            String word = sc.nextLine(); // Take user input
+
+            System.out.println("Salted Output: " + addSalt(word)); // Print the salted result
+        }
+    }
